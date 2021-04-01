16917ba375b3939ee7dca3befaf14d048484046c
PechenkoVkysnoe <1mishagrigorchuk1@mail.ru>
2021-04-01 13:34:01 +0300

    .

---
+++ lab4.1/lab4/ConsoleApp7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    class Program
+    { 
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(int i);
+        static void Main()
+        {
+            while (true)
+            {
+                for (int i = 0; i < 255; i++)
+                {
+                    if (GetAsyncKeyState(i) == 32769)
+                    {
+                        Console.Write((Keys)i);
+                    }
+                }
+                System.Threading.Thread.Sleep(10);
+            }
+        }
+    }
+}
